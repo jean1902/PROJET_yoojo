615281cde6ab9f1fd11f86f01989fee85e3eb5c6
jean1902 <cin71781@gmail.com>
2022-01-28 02:01:01 +0100

    dernier commit

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,36 @@
+
+$('.section2').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+  let btn_prev =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
+  <img src="/source/image/left-arrow_left.png" alt="left_arrow" class="size-arrow_section5">
+</div>
+`
+let btn_next =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
+  <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
+</div>
+`
+
+
+
+
+
+
 $('.section5').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: false,
     // autoplaySpeed: 1000,
   });
-  let btn_prev =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
-  <img src="/source/image/left-arrow_left.png" alt="left_arrow" class="size-arrow_section5">
+  let btn_next1 =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
+  <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
 </div>
 `
-let btn_next =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
+let btn_next2 =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
   <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
 </div>
 `
