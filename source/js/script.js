041098b9d1afd710d70cf6cf9cf5a5881e050ea2
jean1902 <cin71781@gmail.com>
2022-01-28 16:01:29 +0100

    01282022

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,19 @@
 
-$('.section2').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
-});
-  let btn_prev =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
-  <img src="/source/image/left-arrow_left.png" alt="left_arrow" class="size-arrow_section5">
-</div>
-`
-let btn_next =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
-  <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
-</div>
-`
+// $('.container-section2').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
+//   let btn_prev =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
+//   <img src="/source/image/left-arrow_left.png" alt="left_arrow" class="size-arrow_section5">
+// </div>
+// `
+// let btn_next =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
+//   <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
+// </div>
+
 
 
 
@@ -21,16 +21,18 @@ let btn_next =document.querySelector('.slick-next').innerHTML=`<div class="arrow
 
 
 $('.section5').slick({
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: false,
     // autoplaySpeed: 1000,
   });
-  let btn_next1 =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
-  <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
-</div>
+
+  let btn_prev =document.querySelector('.slick-prev').innerHTML=`<div class="arrow_section5 ">
+    <img src="/source/image/left-arrow_left.png" alt="left_arrow" class="size-arrow_section5">
+  </div>
 `
-let btn_next2 =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
-  <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
-</div>
-`
+  let btn_next =document.querySelector('.slick-next').innerHTML=`<div class="arrow_section5 ">
+    <img src="/source/image/left-arrow_right.png" alt="left_arrow" class="size-arrow_section5" >
+  </div>`
+  
+ 
